f2274ebb7ed6cd3575e8ddd6eed003f75ccee2c2
Zequee98 <ezequiel_grox@hotmail.com>
2019-11-06 12:48:28 -0300

    arreglando seed

--- back/server.js
+++ back/server.js
@@ -16,7 +16,7 @@ app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "public"));
 });
 
-db.sync({ force: true })
+db.sync({ force: false })
   .then(() => {
     app.listen(3000, () => {
       console.log("listening on port 3000");
