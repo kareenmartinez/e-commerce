e2553fe231cd51e466958267e4ab56434bed2c0e
ssd4045 <n.s_61059032_cdi@hotmail.com>
2019-11-07 12:20:08 -0300

    base para el registro

--- front/src/containers/App.js
+++ front/src/containers/App.js
@@ -1,13 +1,19 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+import { RegisterPage } from "../components/RegisterPage";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>hola</h1>
-      </div>
-    );
-  }
+function App() {
+  console.log("renderizo App");
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/prueba" component={Prueba} />
+        <Route exact path="/signup" component={RegisterPage} />
+      </Switch>
+
+      <h1>hola</h1>
+    </div>
+  );
 }
 
 export default App;
---
+++ front/src/containers/RegisterContainer.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class RegisterContainer extends Component {
+  render() {
+    return <div></div>;
+  }
+}
