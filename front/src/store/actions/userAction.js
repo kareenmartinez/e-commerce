01e2c08129ff5eaab94970888ea4dc4483380fcf
ssd4045 <n.s_61059032_cdi@hotmail.com>
2019-11-12 11:27:16 -0300

    comiteaaando

--- front/src/store/actions/userAction.js
+++ front/src/store/actions/userAction.js
@@ -8,24 +8,12 @@ const logUser = user => {
     user: user
   };
 };
-<<<<<<< HEAD
-export const logIn = (email, password) => dispatch =>
-  axios
-    .post("/api/logIn", { email: email, password: password })
-    .then(res => res.data)
-    .then(user => {
-      return dispatch(logUser(user));
-    })
-    .catch(err => {
-      console.log(err, "hola, soy tu dolor de cabeza");
-    });
-=======
-const fetcheo = (user) => {
+const fetcheo = user => {
   return {
     type: FETCH_USER,
     user: user
-  }
-}
+  };
+};
 export const logIn = (email, password) => dispatch => ({
   type: LOG_IN,
   payload: axios
@@ -33,19 +21,19 @@ export const logIn = (email, password) => dispatch => ({
     .then(res => res.data)
     .then(user => {
       dispatch(logUser(user));
-
     })
     .catch(err => {
       console.log(err, "hola, soy tu dolor de cabeza");
     })
 });
 export const fetchUser = () => dispatch => ({
-  payload: axios.get("/api/auth/me").then(res => res.data).then(user => {
-    dispatch(fetcheo(user))
-  }).catch(err => {
-    console.log(err, "hola, necesito un abrazo");
-  })
-}
-
-)
->>>>>>> aa36e3e846cfc987cfe6cb4e281960eb89351f93
+  payload: axios
+    .get("/api/auth/me")
+    .then(res => res.data)
+    .then(user => {
+      dispatch(fetcheo(user));
+    })
+    .catch(err => {
+      console.log(err, "hola, necesito un abrazo");
+    })
+});
--- front/src/store/reducers/userReducer.js
+++ front/src/store/reducers/userReducer.js
@@ -1,23 +1,12 @@
 import { LOG_IN, FETCH_USER } from "../constants";
 
 const initialState = {
-
   user: []
 };
 
 export default (state = initialState, actions) => {
   console.log("ENTRE AL REDUCER");
   switch (actions.type) {
-<<<<<<< HEAD
-    case "LOG_USER":
-      return {
-        ...state,
-        isFetching: false,
-        didInvalidate: true,
-        user: actions.user
-      };
-=======
-
     case LOG_IN:
       return {
         ...state,
@@ -33,7 +22,6 @@ export default (state = initialState, actions) => {
         user: actions.user
       };
 
->>>>>>> aa36e3e846cfc987cfe6cb4e281960eb89351f93
     default:
       return state;
   }
